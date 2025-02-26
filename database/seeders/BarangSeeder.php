7825557c50d07ee88c1a46445621b37ed45dc9d5
azzazhr <azzahraqina70@gmail.com>
2025-02-26 21:54:57 +0700

    add praktikum 3

--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -14,23 +14,23 @@ class BarangSeeder extends Seeder
     public function run(): void
     {
         $data = [
-            ['kategori_id' => 1, 'supplier_id' => 1, 'barang_nama' => 'Laptop Asus', 'harga_beli' => 7000000, 'harga_jual' => 8000000],
-            ['kategori_id' => 1, 'supplier_id' => 1, 'barang_nama' => 'Smartphone Samsung', 'harga_beli' => 5000000, 'harga_jual' => 6000000],
-            ['kategori_id' => 2, 'supplier_id' => 1, 'barang_nama' => 'Meja Kayu', 'harga_beli' => 500000, 'harga_jual' => 700000],
-            ['kategori_id' => 2, 'supplier_id' => 1, 'barang_nama' => 'Kursi Kantor', 'harga_beli' => 400000, 'harga_jual' => 600000],
-            ['kategori_id' => 3, 'supplier_id' => 1, 'barang_nama' => 'Sepatu Adidas', 'harga_beli' => 600000, 'harga_jual' => 750000],
+            ['kategori_id' => 1, 'barang_nama' => 'Laptop Asus', 'harga_beli' => 7000000, 'harga_jual' => 8000000],
+            ['kategori_id' => 1, 'barang_nama' => 'Smartphone Samsung', 'harga_beli' => 5000000, 'harga_jual' => 6000000],
+            ['kategori_id' => 2, 'barang_nama' => 'Meja Kayu', 'harga_beli' => 500000, 'harga_jual' => 700000],
+            ['kategori_id' => 2, 'barang_nama' => 'Kursi Kantor', 'harga_beli' => 400000, 'harga_jual' => 600000],
+            ['kategori_id' => 3, 'barang_nama' => 'Sepatu Adidas', 'harga_beli' => 600000, 'harga_jual' => 750000],
 
-            ['kategori_id' => 3, 'supplier_id' => 2, 'barang_nama' => 'Sandal Eiger', 'harga_beli' => 200000, 'harga_jual' => 300000],
-            ['kategori_id' => 4, 'supplier_id' => 2, 'barang_nama' => 'Jaket Jeans', 'harga_beli' => 350000, 'harga_jual' => 500000],
-            ['kategori_id' => 4, 'supplier_id' => 2, 'barang_nama' => 'Kaos Polo', 'harga_beli' => 150000, 'harga_jual' => 250000],
-            ['kategori_id' => 5, 'supplier_id' => 2, 'barang_nama' => 'Kopi Arabica', 'harga_beli' => 80000, 'harga_jual' => 120000],
-            ['kategori_id' => 5, 'supplier_id' => 2, 'barang_nama' => 'Susu UHT', 'harga_beli' => 10000, 'harga_jual' => 15000],
+            ['kategori_id' => 3, 'barang_nama' => 'Sandal Eiger', 'harga_beli' => 200000, 'harga_jual' => 300000],
+            ['kategori_id' => 4, 'barang_nama' => 'Jaket Jeans', 'harga_beli' => 350000, 'harga_jual' => 500000],
+            ['kategori_id' => 4, 'barang_nama' => 'Kaos Polo', 'harga_beli' => 150000, 'harga_jual' => 250000],
+            ['kategori_id' => 5, 'barang_nama' => 'Kopi Arabica', 'harga_beli' => 80000, 'harga_jual' => 120000],
+            ['kategori_id' => 5, 'barang_nama' => 'Susu UHT', 'harga_beli' => 10000, 'harga_jual' => 15000],
 
-            ['kategori_id' => 5, 'supplier_id' => 3, 'barang_nama' => 'Mie Instan', 'harga_beli' => 3000, 'harga_jual' => 5000],
-            ['kategori_id' => 5, 'supplier_id' => 3, 'barang_nama' => 'Beras Premium 5kg', 'harga_beli' => 60000, 'harga_jual' => 75000],
-            ['kategori_id' => 5, 'supplier_id' => 3, 'barang_nama' => 'Minyak Goreng 1L', 'harga_beli' => 20000, 'harga_jual' => 25000],
-            ['kategori_id' => 1, 'supplier_id' => 3, 'barang_nama' => 'Headset Gaming', 'harga_beli' => 250000, 'harga_jual' => 350000],
-            ['kategori_id' => 2, 'supplier_id' => 3, 'barang_nama' => 'Lemari Pakaian', 'harga_beli' => 1000000, 'harga_jual' => 1200000],
+            ['kategori_id' => 5, 'barang_nama' => 'Mie Instan', 'harga_beli' => 3000, 'harga_jual' => 5000],
+            ['kategori_id' => 5, 'barang_nama' => 'Beras Premium 5kg', 'harga_beli' => 60000, 'harga_jual' => 75000],
+            ['kategori_id' => 5, 'barang_nama' => 'Minyak Goreng 1L', 'harga_beli' => 20000, 'harga_jual' => 25000],
+            ['kategori_id' => 1, 'barang_nama' => 'Headset Gaming', 'harga_beli' => 250000, 'harga_jual' => 350000],
+            ['kategori_id' => 2, 'barang_nama' => 'Lemari Pakaian', 'harga_beli' => 1000000, 'harga_jual' => 1200000],
         ];
 
         DB::table('m_barang')->insert($data);
--- database/seeders/StokSeeder.php
+++ database/seeders/StokSeeder.php
@@ -14,12 +14,19 @@ class StokSeeder extends Seeder
      */
     public function run(): void
     {
+        $barangIds = DB::table('m_barang')->pluck('barang_id')->toArray(); 
+
+        if (empty($barangIds)) {
+            return; // Jangan insert jika tidak ada barang
+        }
+
         $data = [];
 
         for ($i = 1; $i <= 15; $i++) {
             $data[] = [
-                'barang_id' => $i,
+                'barang_id' => $barangIds[array_rand($barangIds)],
                 'user_id' => rand(1, 3),
+                'supplier_id' => rand(1, 3),
                 'stok_jumlah' => rand(10, 100),
                 'stok_tanggal' => Carbon::now(),
             ];
