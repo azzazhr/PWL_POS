bca9d7eb153978fcfbc69ca979176af8d8b3e979
azzazhr <azzahraqina70@gmail.com>
2025-03-09 15:17:10 +0700

    add Praktikum 4 - Jobsheet 5

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -15,6 +15,23 @@
             @if (session('error'))
                 <div class="alert alert-danger">{{ session('error') }}</div>
             @endif
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group row">
+                        <label class="col-1 control-label col-form-label">Filter:</label>
+                        <div class="col-3">
+                            <select class="form-control" id="level_id" name="level_id" required>
+                                <option value="">- Semua -</option>
+                                @foreach($level as $item)
+                                    <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                                @endforeach
+                            </select>
+                            <small class="form-text text-muted">Level Pengguna</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
                 <thead>
                     <tr>
@@ -27,30 +44,36 @@
                 </thead>
             </table>
         </div>
-        </div>
+    </div>
 @endsection
-        
-        @push('css')
-        @endpush
-        
-        @push('js')
-            <script>
-                $(document).ready(function () {
-                    var dataUser = $('#table_user').DataTable({
-                        serverSide: true,
-                        ajax: {
-                            url: "{{ url('user/list') }}",
-                            dataType: "json",
-                            type: "POST"
-                        },
-                        columns: [
-                            { data: "DT_RowIndex", className: "text-center", orderable: false, searchable: false },
-                            { data: "username", className: "", orderable: true, searchable: true },
-                            { data: "nama", className: "", orderable: true, searchable: true },
-                            { data: "level.level_nama", className: "", orderable: false, searchable: false },
-                            { data: "aksi", className: "", orderable: false, searchable: false }
-                        ]
-                    });
-                });
-            </script>
-        @endpush
+
+@push('css')
+@endpush
+
+@push('js')
+    <script>
+        $(document).ready(function () {
+            var dataUser = $('#table_user').DataTable({
+                serverSide: true,
+                ajax: {
+                    url: "{{ url('user/list') }}",
+                    dataType: "json",
+                    type: "POST",
+                    data: function (d) {
+                        d.level_id = $('#level_id').val();
+                    }
+                },
+                columns: [
+                    { data: "DT_RowIndex", className: "text-center", orderable: false, searchable: false },
+                    { data: "username", className: "", orderable: true, searchable: true },
+                    { data: "nama", className: "", orderable: true, searchable: true },
+                    { data: "level.level_nama", className: "", orderable: false, searchable: false },
+                    { data: "aksi", className: "", orderable: false, searchable: false }
+                ]
+            });
+            $('#level_id').on('change', function () {
+                dataUser.ajax.reload();
+            });
+        });
+    </script>
+@endpush
