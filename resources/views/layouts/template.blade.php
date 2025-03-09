f6c69d5b7d27504bc8428d90cb0a979c346edf63
azzazhr <azzahraqina70@gmail.com>
2025-03-09 11:41:33 +0700

    add Praktikum 1 - Jobsheet 5

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -28,7 +28,7 @@
       <a href="{{url('/')}}" class="brand-link">
         <img src="{{asset('adminlte/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo"
           class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <span class="brand-text font-weight-light">PWL - Starter Code</span>
       </a>
 
       <!-- Sidebar -->
