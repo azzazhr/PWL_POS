ed5a4d1aec99b75b3e8e3df0106464ef76854041
azzazhr <azzahraqina70@gmail.com>
2025-03-10 20:12:07 +0700

    modifikasi sidebar(tambah menu supplier) - TugasPraktikum m_supplier

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -36,6 +36,11 @@
                         class="nav-icon far fa-list-alt"></i>
                     <p>Data Barang</p>
                 </a> </li>
+                <li class="nav-header">Data Supplier</li>
+                <li class="nav-item"> <a href="{{ url('/supplier') }}"
+                        class="nav-link {{ ($activeMenu == 'supplier') ? 'active' : '' }} "> <i class="nav-icon far fa-bookmark"></i>
+                        <p>Supplier</p>
+                    </a> </li>
             <li class="nav-header">Data Transaksi</li>
             <li class="nav-item"> <a href="{{ url('/stok') }}"
                     class="nav-link {{ ($activeMenu == 'stok') ? 'active' : '' }} "> <i
