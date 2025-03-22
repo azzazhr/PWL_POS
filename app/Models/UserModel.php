f13dd09664bea5875166785ee9d5797f9adae23c
azzazhr <azzahraqina70@gmail.com>
2025-03-22 20:34:56 +0700

    add Praktikum 1 - Jobsheet 7

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,18 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use App\Models\LevelModel;
-use Monolog\Level;
+use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
 
+    //protected $table = 'm_user';
+    //protected $primaryKey = 'user_id';
+    //protected $fillable = ['level_id', 'username', 'nama', 'password'];
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
+    protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
 
+    protected $hidden = ['password']; // jangan ditampilkan saat select
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
