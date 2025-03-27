9bc8972f9405e444aff8c02b41800990178a2938
azzazhr <azzahraqina70@gmail.com>
2025-03-27 20:00:38 +0700

    update NEW : Tugas4JB7 - Implementasi register user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,8 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\LevelModel;
+use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 
 class AuthController extends Controller
@@ -39,6 +43,55 @@ class AuthController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
+        return redirect('login');
+    }
+
+    public function register()
+    {
+        if (Auth::check()) {
+            return redirect('/');
+        }
+
+        $level = LevelModel::select('level_id', 'level_nama')->get();
+
+        return view('auth.register')->with('level', $level);
+    }
+
+    public function postRegister(Request $request)
+    {
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'level_id' => 'required|integer',
+                'username' => 'required|string|min:3|unique:m_user,username',
+                'nama' => 'required|string|max:100',
+                'password' => 'required|min:4'
+            ];
+
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validasi Gagal',
+                    'msgField' => $validator->errors(),
+                ]);
+            }
+
+            // Simpan user ke database
+            UserModel::create([
+                'level_id' => $request->level_id,
+                'username' => $request->username,
+                'nama' => $request->nama,
+                'password' => bcrypt($request->password) // Enkripsi password
+            ]);
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Registrasi berhasil, silakan login.',
+                'redirect' => url('login') // Redirect ke halaman login
+            ]);
+        }
+
         return redirect('login');
     }
 }
