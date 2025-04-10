4d6668d20a1aec6aae8ce2b97c48291fbdb8e879
azzazhr <azzahraqina70@gmail.com>
2025-04-10 19:35:54 +0700

    add Tugas1JB8 - Implementasi import data(user)

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,8 @@ Route::middleware(['auth'])->group(function() { // artinya semua route di dalam 
         Route::get('/user/{id}/delete_ajax', [UserController::class, 'confirm_ajax']);
         Route::delete('/user/{id}/delete_ajax', [UserController::class, 'delete_ajax']);
         Route::delete('/user/{id}', [UserController::class, 'destroy']);
+        Route::get('/user/import', [UserController::class, 'import']);
+        Route::post('/user/import_ajax', [UserController::class, 'import_ajax']);
     });
 
     Route::middleware(['authorize:ADM'])->group(function () {
