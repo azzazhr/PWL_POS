61a5ad0ac848d87c4985dd07a69cb466306d9c5a
azzazhr <azzahraqina70@gmail.com>
2025-04-10 22:17:06 +0700

    modifikasi pada import supplier

--- resources/views/supplier/import.blade.php
+++ resources/views/supplier/import.blade.php
@@ -2,7 +2,7 @@
     @csrf
     <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Import Data supplier User</h5>
+            <h5 class="modal-title" id="exampleModalLabel">Import Data Supplier</h5>
             <button type="button" class="close" data-dismiss="modal" arialabel="Close"><span
                     aria-hidden="true">&times;</span></button>
         </div>
