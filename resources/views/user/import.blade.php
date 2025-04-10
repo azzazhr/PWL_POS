9410b1538cadeefbab852c702ff95ec303ea560b
azzazhr <azzahraqina70@gmail.com>
2025-04-10 22:16:39 +0700

    modifikasi pada import user

--- resources/views/user/import.blade.php
+++ resources/views/user/import.blade.php
@@ -2,7 +2,7 @@
     @csrf
     <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Import Data user User</h5>
+            <h5 class="modal-title" id="exampleModalLabel">Import Data User</h5>
             <button type="button" class="close" data-dismiss="modal" arialabel="Close"><span
                     aria-hidden="true">&times;</span></button>
         </div>
