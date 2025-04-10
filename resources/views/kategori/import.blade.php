b9d260f22cf31ec813e41bcbb428b8b66a9390ed
azzazhr <azzahraqina70@gmail.com>
2025-04-10 22:16:52 +0700

    modifikasi pada import kategori

--- resources/views/kategori/import.blade.php
+++ resources/views/kategori/import.blade.php
@@ -2,7 +2,7 @@
     @csrf
     <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Import Kategori</h5>
+            <h5 class="modal-title" id="exampleModalLabel">Import Data Kategori</h5>
             <button type="button" class="close" data-dismiss="modal" arialabel="Close"><span
                     aria-hidden="true">&times;</span></button>
         </div>
