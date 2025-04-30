c6d82eba831c22d83aa7a78cf478075f8000479d
azzazhr <azzahraqina70@gmail.com>
2025-04-30 12:04:38 +0700

    add Praktikum 1 - Jobsheet 10  # Please enter the commit message for your changes. Lines starting

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,36 +5,48 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
-
-class UserModel extends Authenticatable
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+class UserModel extends Authenticatable implements JWTSubject
 {
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
     use HasFactory;
+
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
+
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
 
-    protected $hidden = ['password']; // jangan ditampilkan saat select
+    protected $hidden = ['password'];
+    protected $casts = ['password' => 'hashed'];
 
-    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+    public function stok()
+    {
+        return $this->hasMany(StokModel::class, 'user_id', 'user_id');
     }
 
     public function getRoleName(): string
     {
         return $this->level->level_nama;
     }
-
     public function hasRole($role): bool
     {
         return $this->level->level_kode == $role;
     }
-
-    public function getRole()
+    public function getRole(): string
     {
         return $this->level->level_kode;
     }
-
 }
