55097ba695234acf112db53738e0fa973550f1eb
azzazhr <azzahraqina70@gmail.com>
2025-05-01 14:19:49 +0700

    add Praktikum 1 - Jobsheet 11

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -18,12 +19,19 @@ class UserModel extends Authenticatable implements JWTSubject
         return [];
     }
 
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => url('/storage/posts/' . $image),
+        );
+    }
+
     use HasFactory;
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password', 'image'];
 
     protected $hidden = ['password'];
     protected $casts = ['password' => 'hashed'];
